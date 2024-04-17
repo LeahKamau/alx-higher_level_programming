9da3bede7210bb3b964b01e106377b05f59001b9
LeahKamau <kamauleah35@gmail.com>
2024-04-16 23:52:36 -0700

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class with method area"""
+
+
+class BaseGeometry:
+    """class with method area that raises exception"""
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
